ef42d16006387ce2a31d67070b1f2e4456e4aac9
Jan Daahll <janwichmann1@gmx.de>
2023-05-14 19:15:21 +0200

    Added the Tables for all question types

--- Flask/creat_db.py
+++ Flask/creat_db.py
@@ -25,42 +25,76 @@ CREATE TABLE fragen (
     fragen_typ VARCHAR(255) NOT NULL,
     fragen_id INTEGER NOT NULL
 );
+
+-- Create the aufgabenstellungen table
+CREATE TABLE aufgabenstellungen (
+    id SERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    aufgabentyp TEXT NOT NULL
+);
             
 -- Create the Trainings table
 CREATE TABLE trainings (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    question_id_1 INTEGER,
-    question_id_2 INTEGER,
-    question_id_3 INTEGER,
-    question_id_4 INTEGER,
-    question_id_5 INTEGER,
-    question_id_6 INTEGER,
-    question_id_7 INTEGER,
-    question_id_8 INTEGER,
-    question_id_9 INTEGER,
-    question_id_10 INTEGER,
-    FOREIGN KEY (question_id_1) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_2) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_3) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_4) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_5) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_6) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_7) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_8) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_9) REFERENCES questions (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_id_10) REFERENCES questions (id) ON DELETE CASCADE
+    fragen_id_1 INTEGER,
+    fragen_id_2 INTEGER,
+    fragen_id_3 INTEGER,
+    fragen_id_4 INTEGER,
+    fragen_id_5 INTEGER,
+    fragen_id_6 INTEGER,
+    fragen_id_7 INTEGER,
+    fragen_id_8 INTEGER,
+    fragen_id_9 INTEGER,
+    fragen_id_10 INTEGER,
+    FOREIGN KEY (fragen_id_1) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_2) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_3) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_4) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_5) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_6) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_7) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_8) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_9) REFERENCES fragen (id) ON DELETE CASCADE,
+    FOREIGN KEY (fragen_id_10) REFERENCES fragen (id) ON DELETE CASCADE
 );
-            -- Create the Proben table
+
+-- Create the Proben table
 CREATE TABLE proben (
     id SERIAL PRIMARY KEY,
     proben_nr INTEGER UNIQUE NOT NULL,
     probenname VARCHAR(255) NOT NULL,
-    aussehen_farbe TEXT NOT NULL,
-    geruch TEXT NOT NULL,
-    geschmack TEXT NOT NULL,
-    textur TEXT NOT NULL,
-    konsistenz TEXT NOT NULL
+    farbe TEXT,
+    farbintensität INTEGER,
+    geruch TEXT,
+    geschmack TEXT,
+    textur TEXT,
+    konsistenz TEXT
+);
+
+-- Create the probenreihen table
+CREATE TABLE probenreihen (
+    id SERIAL PRIMARY KEY,
+    proben_id_1 INTEGER NOT NULL,
+    proben_id_2 INTEGER NOT NULL,
+    proben_id_3 INTEGER NOT NULL,
+    proben_id_4 INTEGER NOT NULL,
+    proben_id_5 INTEGER NOT NULL,
+    proben_id_6 INTEGER NOT NULL,
+    proben_id_7 INTEGER NOT NULL,
+    proben_id_8 INTEGER NOT NULL,
+    proben_id_9 INTEGER NOT NULL,
+    proben_id_10 INTEGER NOT NULL,
+    FOREIGN KEY (proben_id_1) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_2) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_3) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_4) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_5) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_6) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_7) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_8) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_9) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_10) REFERENCES proben (id) ON DELETE CASCADE
 );
 
 -- Create the Benutzer table
@@ -73,49 +107,213 @@ CREATE TABLE benutzer (
     FOREIGN KEY (training_id) REFERENCES trainings (id) ON DELETE CASCADE
 );
 
+-- Create the dreieckstest table
+CREATE TABLE dreieckstest (
+    id SERIAL PRIMARY KEY,
+    title_id INTEGER,
+    prüfvariante TEXT NOT NULL,
+    probenreihe_id INTEGER NOT NULL,
+    proben_auswahl INTEGER,
+    beschreibung TEXT,
+    FOREIGN KEY (probenreihe_id) REFERENCES probenreihen (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
+);
+
+-- Create the konz_reihe table
+CREATE TABLE konz_reihe (
+    id SERIAL PRIMARY KEY,
+    title_id INTEGER,
+    probenreihe_id INTEGER NOT NULL,
+    probe_1_antwort TEXT,
+    probe_2_antwort TEXT,
+    probe_3_antwort TEXT,
+    probe_4_antwort TEXT,
+    probe_5_antwort TEXT,
+    probe_6_antwort TEXT,
+    probe_7_antwort TEXT,
+    probe_8_antwort TEXT,
+    probe_9_antwort TEXT,
+    probe_10_antwort TEXT,
+
+    FOREIGN KEY (probenreihe_id) REFERENCES probenreihen (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
+);
+
+-- Create the profilprüfung table
+CREATE TABLE profilprüfung (
+    id SERIAL PRIMARY KEY,
+    title_id INTEGER,
+    proben_id INTEGER NOT NULL,
+    kriterium_1 TEXT,
+    kriterium_2 TEXT,
+    kriterium_3 TEXT,
+    kriterium_4 TEXT,
+    kriterium_5 TEXT,
+    kriterium_6 TEXT,
+    kriterium_7 TEXT,
+    kriterium_8 TEXT,
+    kriterium_9 TEXT,
+    skalenbewertung_1 INTEGER,
+    skalenbewertung_2 INTEGER,
+    skalenbewertung_3 INTEGER,
+    skalenbewertung_4 INTEGER,
+    skalenbewertung_5 INTEGER,
+    skalenbewertung_6 INTEGER,
+    skalenbewertung_7 INTEGER,
+    skalenbewertung_8 INTEGER,
+    skalenbewertung_9 INTEGER,
+    FOREIGN KEY (proben_id) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
+);
+
+-- Create the hedonische_beurteilung table
+CREATE TABLE hed_beurteilung (
+    id SERIAL PRIMARY KEY,
+    title_id INTEGER,
+    proben_id INTEGER NOT NULL,
+    beurteilung TEXT,
+    anmerkung TEXT,
+    FOREIGN KEY (proben_id) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
+);
+
+-- Create the auswahltest table
+CREATE TABLE auswahltest (
+    id SERIAL PRIMARY KEY,
+    title_id INTEGER,
+    prüfvariante TEXT NOT NULL,
+    proben_id_1 INTEGER NOT NULL,
+    proben_id_2 INTEGER NOT NULL,
+    proben_id_3 INTEGER NOT NULL,
+    proben_id_4 INTEGER NOT NULL,
+    proben_id_5 INTEGER NOT NULL,
+    proben_id_6 INTEGER NOT NULL,
+    bemerkung_1 TEXT,
+    bemerkung_2 TEXT,
+    bemerkung_3 TEXT,
+    bemerkung_4 TEXT,
+    bemerkung_5 TEXT,
+    bemerkung_6 TEXT,
+    FOREIGN KEY (proben_id_1) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_2) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_3) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_4) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_5) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_6) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
+);
+
+-- Create the geruchserkennung table
+CREATE TABLE geruchserkennung (
+    id SERIAL PRIMARY KEY,
+    title_id INTEGER,
+    proben_id INTEGER NOT NULL,
+    geruch_ohne_auswahl TEXT,
+    geruch_mit_auswahl TEXT,
+    bemerkung TEXT,
+    FOREIGN KEY (proben_id) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
+);
+
+-- Create the paar_vergleich table
+CREATE TABLE paar_vergleich (
+    id SERIAL PRIMARY KEY,
+    title_id INTEGER,
+    prüfvariante TEXT NOT NULL,
+    proben_id_1 INTEGER NOT NULL,
+    proben_id_2 INTEGER NOT NULL,
+    proben_auswahl_id TEXT,
+    bemerkung TEXT,
+    FOREIGN KEY (proben_id_1) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (proben_id_2) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
+);
+
 -- Create the EBP table
 CREATE TABLE ebp (
     id SERIAL PRIMARY KEY,
+    title_id INTEGER,
+    prüfvariante TEXT,
     proben_id INTEGER NOT NULL,
     aussehen_farbe TEXT,
     geruch TEXT,
     geschmack TEXT,
     textur TEXT,
     konsistenz TEXT,
-    FOREIGN KEY (proben_id) REFERENCES proben (id) ON DELETE CASCADE
+    FOREIGN KEY (proben_id) REFERENCES proben (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
 );
 
--- Create the Rangordnungstest table
+-- Create the rangordnungstest table
 CREATE TABLE rangordnungstest (
     id SERIAL PRIMARY KEY,
-    proben_id_1 INTEGER NOT NULL,
-    proben_id_2 INTEGER NOT NULL,
-    proben_id_3 INTEGER NOT NULL,
-    proben_id_4 INTEGER NOT NULL,
-    proben_id_5 INTEGER NOT NULL,
-    FOREIGN KEY (proben_id_1) REFERENCES proben (id) ON DELETE CASCADE,
-    FOREIGN KEY (proben_id_2) REFERENCES proben (id) ON DELETE CASCADE,
-    FOREIGN KEY (proben_id_3) REFERENCES proben (id) ON DELETE CASCADE,
-    FOREIGN KEY (proben_id_4) REFERENCES proben (id) ON DELETE CASCADE,
-    FOREIGN KEY (proben_id_5) REFERENCES proben (id) ON DELETE CASCADE
+    title_id INTEGER,
+    prüfvariante TEXT NOT NULL,
+    probenreihe_id INTEGER NOT NULL,
+    rang_1_proben_id INTEGER,
+    rang_2_proben_id INTEGER,
+    rang_3_proben_id INTEGER,
+    rang_4_proben_id INTEGER,
+    rang_5_proben_id INTEGER,
+    FOREIGN KEY (probenreihe_id) REFERENCES probenreihen (id) ON DELETE CASCADE,
+    FOREIGN KEY (title_id) REFERENCES aufgabenstellungen (id) ON DELETE CASCADE
 );
 
--- Add foreign key constraints for the Questions table
-ALTER TABLE questions
-ADD CONSTRAINT fk_questions_ebp
-FOREIGN KEY (question_id)
+-- Add foreign key constraints for the fragens table
+
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_ebp
+FOREIGN KEY (fragen_id)
 REFERENCES ebp (id)
 ON DELETE CASCADE;
 
-ALTER TABLE questions
-ADD CONSTRAINT fk_questions_rangordnungstest
-FOREIGN KEY (question_id)
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_rangordnungstest
+FOREIGN KEY (fragen_id)
 REFERENCES rangordnungstest (id)
 ON DELETE CASCADE;
 
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_paar_vergleich
+FOREIGN KEY (fragen_id)
+REFERENCES paar_vergleich (id)
+ON DELETE CASCADE;
 
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_geruchserkennung
+FOREIGN KEY (fragen_id)
+REFERENCES geruchserkennung (id)
+ON DELETE CASCADE;
 
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_auswahltest
+FOREIGN KEY (fragen_id)
+REFERENCES auswahltest (id)
+ON DELETE CASCADE;
 
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_hed_beurteilung
+FOREIGN KEY (fragen_id)
+REFERENCES hed_beurteilung (id)
+ON DELETE CASCADE;
+
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_profilprüfung
+FOREIGN KEY (fragen_id)
+REFERENCES profilprüfung (id)
+ON DELETE CASCADE;
+
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_konz_reihe
+FOREIGN KEY (fragen_id)
+REFERENCES konz_reihe (id)
+ON DELETE CASCADE;
+
+ALTER TABLE fragen
+ADD CONSTRAINT fk_fragen_dreieckstest
+FOREIGN KEY (fragen_id)
+REFERENCES dreieckstest (id)
+ON DELETE CASCADE;
 
 INSERT INTO public.benutzer(
 	id, benutzername, passwort, rolle, training_id)
@@ -134,8 +332,8 @@ INSERT INTO public.benutzer(
 	VALUES (4, 'Student3', '123', False, NULL);
 
 INSERT INTO public.proben(
-	id, proben_nr, probenname, aussehen_farbe, geruch, geschmack, textur, konsistenz)
-	VALUES (1, 999, 'Schwarzdorn', 'Schwarz', 'erdig', 'salzig', 'rau', 'fest');
+	id, proben_nr, probenname, farbe, farbintensität, geruch, geschmack, textur, konsistenz)
+	VALUES (1, 999, 'Schwarzdorn', 'Schwarz', 100, 'erdig', 'salzig', 'rau', 'fest');
 
             
             
--- Flask/model.py
+++ Flask/model.py
@@ -2,72 +2,187 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Proben(db.Model):
-    __tablename__ = 'proben'
-    id = db.Column(db.Integer, primary_key=True)
-    proben_nr = db.Column(db.Integer, unique=True, nullable=False)
-    probenname = db.Column(db.String(255), nullable=False)
-    aussehen_farbe = db.Column(db.Text, nullable=True)
-    geruch = db.Column(db.Text, nullable=True)
-    geschmack = db.Column(db.Text, nullable=True)
-    textur = db.Column(db.Text, nullable=True)
-    konsistenz = db.Column(db.Text, nullable=True)
+"""
+Allgemeine Tabllen (benutzer, proben, trainings etc.)
+"""
 
-class EBP(db.Model):
-    __tablename__ = 'ebp'
-    id = db.Column(db.Integer, primary_key=True)
-    proben_id = db.Column(db.Integer, db.ForeignKey('proben.id', ondelete='CASCADE'))
-    aussehen_farbe = db.Column(db.Text, nullable=True)
-    geruch = db.Column(db.Text, nullable=True)
-    geschmack = db.Column(db.Text, nullable=True)
-    textur = db.Column(db.Text, nullable=True)
-    konsistenz = db.Column(db.Text, nullable=True)
-
-    probe = db.relationship('Proben', backref=db.backref('ebp', lazy='dynamic'))
-
-class Rangordnungstest(db.Model):
-    __tablename__ = 'rangordnungstest'
-    id = db.Column(db.Integer, primary_key=True)
-    proben_id_1 = db.Column(db.Integer, db.ForeignKey('proben.id', ondelete='CASCADE'))
-    proben_id_2 = db.Column(db.Integer, db.ForeignKey('proben.id', ondelete='CASCADE'))
-    proben_id_3 = db.Column(db.Integer, db.ForeignKey('proben.id', ondelete='CASCADE'))
-    proben_id_4 = db.Column(db.Integer, db.ForeignKey('proben.id', ondelete='CASCADE'))
-    proben_id_5 = db.Column(db.Integer, db.ForeignKey('proben.id', ondelete='CASCADE'))
-
-    probe_1 = db.relationship('Proben', foreign_keys=[proben_id_1], backref=db.backref('rangordnungstest_1', lazy='dynamic'))
-    probe_2 = db.relationship('Proben', foreign_keys=[proben_id_2], backref=db.backref('rangordnungstest_2', lazy='dynamic'))
-    probe_3 = db.relationship('Proben', foreign_keys=[proben_id_3], backref=db.backref('rangordnungstest_3', lazy='dynamic'))
-    probe_4 = db.relationship('Proben', foreign_keys=[proben_id_4], backref=db.backref('rangordnungstest_4', lazy='dynamic'))
-    probe_5 = db.relationship('Proben', foreign_keys=[proben_id_5], backref=db.backref('rangordnungstest_5', lazy='dynamic'))
-
-
-class Questions(db.Model):
+class Fragen(db.model):
     __tablename__ = 'fragen'
+
     id = db.Column(db.Integer, primary_key=True)
     fragen_typ = db.Column(db.String(255), nullable=False)
     fragen_id = db.Column(db.Integer, nullable=False)
 
-class Trainings(db.Model):
+class Aufgabenstellungen(db.model):
+    __tablename__ = 'aufgabenstellungen'
+
+    id = db.Column(db.Integer, primary_key=True)
+    db.Text = db.Column(db.Text, nullable=False)
+    aufgabentyp = db.Column(db.Text, nullable=False)
+
+class Trainings(db.model):
     __tablename__ = 'trainings'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    question_id_1 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_2 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_3 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_4 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_5 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_6 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_7 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_8 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_9 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
-    question_id_10 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
+    fragen_id_1 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
+    fragen_id_2 = db.Column(db.Integer, db.ForeignKey('fragen.id', ondelete='CASCADE'))
+    # ... and so on for the rest of the fragen_ids
 
-class Benutzer(db.Model):
-    __tablename__ = 'benutzer'
+class Proben(db.model):
+    __tablename__ = 'proben'
+
     id = db.Column(db.Integer, primary_key=True)
-    benutzername = db.Column(db.String(255), nullable=False)
-    passwort = db.Column(db.String(255), nullable=False)
+    proben_nr = db.Column(db.Integer, unique=True, nullable=False)
+    probenname = db.Column(db.String(255), nullable=False)
+    farbe = db.Column(db.Text)
+    farbintensität = db.Column(db.Integer)
+    geruch = db.Column(db.Text)
+    geschmack = db.Column(db.Text)
+    db.Textur = db.Column(db.Text)
+    konsistenz = db.Column(db.Text)
+
+class Probenreihen(db.model):
+
+    __tablename__ = 'probenreihen'
+
+    id = db.Column(db.Integer, primary_key=True)
+    proben_id_1 = db.Column(db.Integer, db.ForeignKey('proben.id', ondelete='CASCADE'))
+    # ... and so on for the rest of the proben_ids
+
+class Benutzer(db.model):
+    __tablename__ = 'benutzer'
+
+    id = db.Column(db.Integer, primary_key=True)
+    benutzername = db.Column(db.Text, nullable=False)
+    passwort = db.Column(db.Text, nullable=False)
     rolle = db.Column(db.Boolean, nullable=False)
     training_id = db.Column(db.Integer, db.ForeignKey('trainings.id', ondelete='CASCADE'))
-    training = db.relationship('Trainings', backref=db.backref('benutzer', lazy='dynamic'))
 
+
+"""
+Fragetyp Tabellen (ebp, rangordnungstest, Auswahltest etc.)
+"""
+
+class KonzReihe(db.model):
+    __tablename__ = "konz_reihe"
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    probenreihe_id = db.Column(db.Integer, db.ForeignKey('probenreihen.id'))
+    probe_1_antwort = db.Column(db.String)
+    probe_2_antwort = db.Column(db.String)
+    probe_3_antwort = db.Column(db.String)
+    probe_4_antwort = db.Column(db.String)
+    probe_5_antwort = db.Column(db.String)
+    probe_6_antwort = db.Column(db.String)
+    probe_7_antwort = db.Column(db.String)
+    probe_8_antwort = db.Column(db.String)
+    probe_9_antwort = db.Column(db.String)
+    probe_10_antwort = db.Column(db.String)
+
+class ProfilPrüfung(db.model):
+    __tablename__ = "profilprüfung"
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    proben_id = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    kriterium_1 = db.Column(db.String)
+    kriterium_2 = db.Column(db.String)
+    kriterium_3 = db.Column(db.String)
+    kriterium_4 = db.Column(db.String)
+    kriterium_5 = db.Column(db.String)
+    kriterium_6 = db.Column(db.String)
+    kriterium_7 = db.Column(db.String)
+    kriterium_8 = db.Column(db.String)
+    kriterium_9 = db.Column(db.String)
+    skalenbewertung_1 = db.Column(db.Integer)
+    skalenbewertung_2 = db.Column(db.Integer)
+    skalenbewertung_3 = db.Column(db.Integer)
+    skalenbewertung_4 = db.Column(db.Integer)
+    skalenbewertung_5 = db.Column(db.Integer)
+    skalenbewertung_6 = db.Column(db.Integer)
+    skalenbewertung_7 = db.Column(db.Integer)
+    skalenbewertung_8 = db.Column(db.Integer)
+    skalenbewertung_9 = db.Column(db.Integer)
+
+class HedBeurteilung(db.model):
+    __tablename__ = "hed_beurteilung"
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    proben_id = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    beurteilung = db.Column(db.String)
+    anmerkung = db.Column(db.String)
+
+class Auswahltest(db.model):
+    __tablename__ = "auswahltest"
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    prüfvariante = db.Column(db.String)
+    proben_id_1 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    proben_id_2 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    proben_id_3 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    proben_id_4 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    proben_id_5 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    proben_id_6 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    bemerkung_1 = db.Column(db.String)
+    bemerkung_2 = db.Column(db.String)
+    bemerkung_3 = db.Column(db.String)
+    bemerkung_4 = db.Column(db.String)
+    bemerkung_5 = db.Column(db.String)
+    bemerkung_6 = db.Column(db.String)
+
+class Geruchserkennung(db.model):
+    __tablename__ = "geruchserkennung"
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    probe_id = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    geruch = db.Column(db.String)
+    intensität = db.Column(db.Integer)
+    anmerkung = db.Column(db.String)
+    
+class PaarVergleich(db.model):
+    __tablename__ = 'paar_vergleich'
+    id = db.Column(db.Integer, primary_key=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    prüfvariante = db.Column(db.String)
+    proben_id_1 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    proben_id_2 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    proben_auswahl_id = db.Column(db.String)
+    bemerkung = db.Column(db.String)
+
+class Ebp(db.model):
+    __tablename__ = 'ebp'
+    id = db.Column(db.Integer, primary_key=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    prüfvariante = db.Column(db.String)
+    proben_id = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    aussehen_farbe = db.Column(db.String)
+    geruch = db.Column(db.String)
+    geschmack = db.Column(db.String)
+    textur = db.Column(db.String)
+    konsistenz = db.Column(db.String)
+
+class Rangordnungstest(db.model):
+    __tablename__ = 'rangordnungstest'
+    id = db.Column(db.Integer, primary_key=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    prüfvariante = db.Column(db.String)
+    probenreihe_id = db.Column(db.Integer, db.ForeignKey('probenreihen.id'))
+    rang_1_proben_id = db.Column(db.Integer)
+    rang_2_proben_id = db.Column(db.Integer)
+    rang_3_proben_id = db.Column(db.Integer)
+    rang_4_proben_id = db.Column(db.Integer)
+    rang_5_proben_id = db.Column(db.Integer)
+
+class Dreieckstest(Base):
+    __tablename__ = 'dreieckstest'
+    id = db.Column(db.Integer, primary_key=True)
+    title_id = db.Column(db.Integer, db.ForeignKey('aufgabenstellungen.id'))
+    prüfvariante = db.Column(db.String)
+    proben_id_1 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    proben_id_2 = db.Column(db.Integer, db.ForeignKey('proben.id'))
+    abweichende_probe = db.Column(db.Integer)
